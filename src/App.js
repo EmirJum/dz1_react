65030c009129e86ba72e0010580d0642367b5377
EmirJum <mirkajum@gmail.com>
2022-06-14 15:52:52 +0600

    HW1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css'; 
+ 
+function AboutUs() { 
+	return ( 
+		<div> 
+			<h2> 
+				About Us 
+			</h2> 
+		</div> 
+	) 
+} 
+ 
+function App() { 
+	return ( 
+		<div className="container"> 
+			App js 
+			<AboutUs/> 
+		</div> 
+	); 
+} 
+ 
+export default App;
